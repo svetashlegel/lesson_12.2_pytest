595293567f21e233ac67f502495e251201d62afe
shlegel <sveta.shlegel@mail.ru>
2023-04-26 19:56:53 +0300

    added tests for negative starts

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,5 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3], -10) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -3) == [1, 2, 3]
