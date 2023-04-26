07630f963f394f9b44f43cf9c3a1fb2acf95d4d6
shlegel <sveta.shlegel@mail.ru>
2023-04-26 22:15:01 +0300

    added fixture

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,9 +1,9 @@
 from utils import dicts
 
 
-def test_get_val():
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+def test_get_val(dict_fixture):
+    assert dicts.get_val(dict_fixture, "vcs") == "mercurial"
+    assert dicts.get_val(dict_fixture, "vcs", "git") == "mercurial"
     assert dicts.get_val({}, "vcs", "git") == "git"
     assert dicts.get_val({}, "vcs", "bazar") == "bazar"
-    assert dicts.get_val({"vcs": "mercurial"}, "bazar") == "git"
+    assert dicts.get_val(dict_fixture, "bazar") == "git"
